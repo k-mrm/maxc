d9e165ad1f5c86fd173f6e16da83ced5b7b8c444
mrm <kei.id0305@gmail.com>
2018-12-28 00:16:30 +0900

    crushed parts I disliked

--- lexer.cpp
+++ lexer.cpp
@@ -4,7 +4,7 @@ Token Lexer::lex(std::string src) {
     Token token;
     //std::cout << src << std::endl;
 
-    for(int i = 0; i < src.size() - 1; i++) {
+    for(int i = 0; i < src.size(); i++) {
         if(isdigit(src[i])) {
             std::string value_num;
 
