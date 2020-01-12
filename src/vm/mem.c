ef602b7871270b07df8c9b9e47ca73c1b32effa6
mrm <kei.id0305@gmail.com>
2020-01-12 19:42:22 +0900

    delete duplicated code

--- src/vm/mem.c
+++ src/vm/mem.c
@@ -5,8 +5,7 @@ ObjectPool obpool;
 
 void New_Objectpool() {
     obpool.pool = malloc(sizeof(MxcObject *) * 128);
-    obpool.len = 128;
-    obpool.reserved = 128;
+    obpool.reserved = obpool.len = 128;
 
     for(int i = 0; i < 128; ++i) {
         obpool.pool[i] = malloc(sizeof(union obalign));
@@ -28,22 +27,12 @@ void obpool_push(MxcObject *ob) {
 
 static MxcObject *obpool_pop() { return obpool.pool[--obpool.len]; }
 
-static void obpool_realloc() {
-    obpool.pool = malloc(sizeof(MxcObject *) * 128);
-    obpool.len = 128;
-    obpool.reserved = 128;
-
-    for(int i = 0; i < 128; ++i) {
-        obpool.pool[i] = (MxcObject *)malloc(sizeof(union obalign));
-    }
-}
-
 #endif
 
 MxcObject *Mxc_malloc(size_t s) {
 #ifdef OBJECT_POOL
     if(obpool.len == 0) {
-        obpool_realloc();
+        New_Objectpool();
     }
 
     MxcObject *ob = obpool_pop();
